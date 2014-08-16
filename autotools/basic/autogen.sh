0bea1589e7126e11f6c3143f0e37b8a6f9633d15
Kamil Prusko <kamilprusko@gmail.com>
2014-08-16 14:31:49 +0200

    Add basic example for autotools and glib

---
+++ autotools/basic/autogen.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# Run this to generate all the initial makefiles, etc.
+
+PKG_NAME=example
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory \"\'$srcdir\'\" does not look like the"
+    echo " top-level package directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common!"
+    exit 1
+}
+
+REQUIRED_AUTOMAKE_VERSION=1.9 GNOME_DATADIR="$gnome_datadir" . gnome-autogen.sh
---
+++ autotools/basic/src/main.c
@@ -0,0 +1,9 @@
+#include <glib.h>
+
+
+int main (int argc, char** argv)
+{
+    g_message ("Hello Wrold!");
+
+    return 0;
+}
